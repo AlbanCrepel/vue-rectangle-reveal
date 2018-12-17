2da5ba5bc1f7283e580fd193c6b86c85295cae9f
Alban Crepel <alban.crepel@gmail.com>
2018-12-17 16:22:23 +0100

    :point_up: first commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# package
+> 
+
+## Demo
+
+<p align="center"> 
+    <img src="./src/assets/demo.gif" width="400px" height="auto" alt="Demo gif"/>
+</p>
+
+## Live demo
+
+Coming soon
+
+## Installation
+
+#### NPM
+
+```bash
+ npm i --save package
+ ```
+
+## Example
+
+```vue
+    
+```
+
+## Props
+
+| name         |  type  | description                                                      | required | default value |
+|--------------|:------:|------------------------------------------------------------------|----------|---------------|
+|        |   |      |      |   |
+
+## Methods
+
+| Name                    | Description             |Params|Returns
+|-------------------------|-------------------------|----------|----------|
+|                     |     |      |    |
+
+## Events emitted
+
+| Name                    | Description             |
+|-------------------------|-------------------------|
+|                     |  |
+
+
+## Contributing 
+
+Contributions are welcome !
+Follow the instructions in the [contributing file](./CONTRIBUTING.md)
+
+## License
+
+[MIT](./LICENCE)
+
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+var config = {
+    output: {
+        path: path.resolve(__dirname + '/dist/'),
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                include: __dirname,
+                exclude: /node_modules/
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.css$/,
+                loader: 'style!less!css'
+            },
+        ]
+    },
+    externals: {
+        vue: 'vue'
+    },
+    plugins: [
+        new UglifyJsPlugin( {
+            uglifyOptions : {
+                minimize : true,
+                sourceMap : false,
+                mangle: true,
+                compress: {
+                    warnings: false
+                }
+            }
+        } )
+    ]
+};
+
+
+module.exports = [
+    merge(config, {
+        entry: path.resolve(__dirname + '/src/Component.vue'),
+        output: {
+            filename: 'package.js',
+            libraryTarget: 'umd',
+            library: 'name',
+            umdNamedDefine: true
+        }
+    })
+];
